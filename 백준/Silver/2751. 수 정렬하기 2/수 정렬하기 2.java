2e17cff56a90d8bcd1bd694e51133dbdb35aae53
seunghyun <140399766+1winhyun@users.noreply.github.com>
2025-02-07 22:02:03 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 4528 ms, Memory: 197768 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -1,23 +1,59 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
+    public static int[] sorted, A;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
 
-        int[] A = new int[N];
-        for(int i=0;i<N;i++){
+        A = new int[N];
+        sorted = new int[N];
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             A[i] = Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(A);
+        merge_sort(A, 0, N - 1);
         for(int i=0;i<N;i++){
             System.out.println(A[i]);
+        }
+    }
+
+    public static void merge_sort(int[] A, int start, int end) {
+        if (start < end) {
+            int mid = (start + end) / 2;
+            merge_sort(A, start, mid);
+            merge_sort(A, mid + 1, end);
+            merge(A, start, mid, end);
+        }
+    }
+
+    public static void merge(int[] A, int start, int mid, int end) {
+        int i = start;
+        int j = mid + 1;
+        int k = start;
+        while (i <= mid && j <= end) {
+            if (A[i] > A[j]) {
+                sorted[k++] = A[j++];
+            } else {
+                sorted[k++] = A[i++];
+            }
+        }
+        if (i > mid) {
+            for (int l = j; l <= end; l++) {
+                sorted[k++] = A[l];
+            }
+        } else {
+            for (int l = i; l <= mid; l++) {
+                sorted[k++] = A[l];
+            }
+        }
+        for (int l = start; l <= end; l++) {
+            A[l] = sorted[l];
         }
     }
 }
