d7338a1e6663700d12fd3f899a7e5992d77124dd
seunghyun <140399766+1winhyun@users.noreply.github.com>
2025-02-13 15:13:48 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 2524 ms, Memory: 340252 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -1,8 +1,9 @@
 import java.io.*;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
+    public static int[] n = {};
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -10,19 +11,43 @@ public class Main {
 
         int N = Integer.parseInt(st.nextToken());
 
-        int[] number = new int[N];
+        n = new int[N];
 
-        for(int i=0;i<N;i++){
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            number[i] = Integer.parseInt(st.nextToken());
+            n[i] = Integer.parseInt(st.nextToken());
         }
 
-        Arrays.sort(number);
+        radix_sort(n);
 
-        for(int i=0;i<N;i++){
-            bw.write(number[i]+"\n");
+        for (int i = 0; i < N; i++) {
+            bw.write(n[i]+"\n");
         }
         bw.flush();
         bw.close();
+    }
+
+    public static void radix_sort(int[] n) {
+        int jari = 1;
+        int count = 0;
+        int[] output = new int[n.length];
+        while (count != 5) {
+            int[] bucket = new int[10];
+            for (int i = 0; i < n.length; i++) {
+                bucket[(n[i] / jari) % 10]++;
+            }
+            for (int i = 1; i < 10; i++) {
+                bucket[i] += bucket[i - 1];
+            }
+            for(int i=n.length-1;i>=0;i--){
+                output[bucket[(n[i]/jari)%10]-1]=n[i];
+                bucket[(n[i]/jari)%10]--;
+            }
+            for(int i=0;i<n.length;i++){
+                n[i]=output[i];
+            }
+            count++;
+            jari *= 10;
+        }
     }
 }
